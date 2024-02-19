b84f1d8ba66fc8cf1d27ba571d60f142db499754
averythurston <a.j.m.thurston@gmail.com>
2024-02-19 16:59:25 -0500

    adding these files to git

---
+++ jeopardy.c
@@ -0,0 +1,144 @@
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "questions.h"
+#include "players.h"
+#include "jeopardy.h"
+
+// Put macros or constants here using #define
+#define BUFFER_LEN 256
+#define NUM_PLAYERS 4
+
+// Put global environment variables here
+
+// Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
+void tokenize(char *input, char **tokens){
+    int token_count = 0;
+
+    char *token = strtok(input, " ");
+    while (token != NULL) {
+        tokens[token_count] = malloc(strlen(token) + 1);
+        if (tokens[token_count] == NULL) {
+            perror("Failed to allocate memory");
+            exit(EXIT_FAILURE);
+        }
+
+        strcpy(tokens[token_count], token);
+
+        token = strtok(NULL, " ");
+        token_count++;
+    }
+
+    tokens[token_count] = NULL;
+}
+
+
+// Displays the game results for each player, their name and final score, ranked from first to last place
+void show_results(player *players, int num_players){
+    for (int i = 0; i < num_players - 1; i++) {
+        for (int j = 0; j < num_players - i - 1; j++) {
+            if (players[j].score < players[j + 1].score) {
+                player temp = players[j];
+                players[j] = players[j + 1];
+                players[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Game Results:\n");
+    for (int i = 0; i < num_players; i++) {
+        printf("Rank %d: %s - Score: %d\n", i + 1, players[i].name, players[i].score);
+    }
+}
+
+bool is_game_over(void) {
+    for (int i = 0; i < NUM_CATEGORIES; i++) {
+        for (int j = 0; j < NUM_VALUES; j++) {
+            if (!questions[i][j].answered) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    // An array of 4 players, may need to be a pointer if you want it set dynamically
+    player players[NUM_PLAYERS];
+    
+    // Input buffer and and commands
+    char buffer[BUFFER_LEN] = { 0 };
+
+    // Display the game introduction and initialize the questions
+    initialize_game();
+
+    // Prompt for players names
+    printf("Enter the names of the players:\n");
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+        printf("Player %d: ", i + 1);
+        scanf("%s", players[i].name);
+        players[i].score = 0;
+    }
+    // initialize each of the players in the array
+
+    // Perform an infinite loop getting command input from users until game ends
+    while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
+    {
+        // Call functions from the questions and players source files
+        display_categories();
+
+        // Execute the game until all questions are answered
+        printf("Enter player name to select a question: ");
+        char selected_player[50];
+        scanf("%s", selected_player);
+        getchar();
+
+        if (!player_exists(players, NUM_PLAYERS, selected_player)) {
+            printf("Invalid player!\n");
+            continue;
+        }
+
+        char selected_category[50];
+        int selected_value;
+        printf("Enter category and dollar amount: ");
+        scanf("%s %d", selected_category, &selected_value);
+
+        if (already_answered(selected_category, selected_value)) {
+            printf("Question has already been answered!\n");
+            continue;
+        }
+
+        display_question(selected_category, selected_value);
+
+        char answer[BUFFER_LEN];
+        printf("Enter your answer: ");
+        fgets(answer, BUFFER_LEN, stdin);
+        strtok(answer, "\n");  
+
+        if (valid_answer(selected_category, selected_value, answer)) {
+            printf("Correct!\n");
+            update_score(players, NUM_PLAYERS, selected_player, selected_value);
+        } else {
+            printf("Incorrect!\n");
+        }
+
+        already_answered(selected_category, selected_value);
+
+        if (is_game_over()) {
+            break;
+        }
+        // Display the final results and exit
+    }
+    show_results(players, NUM_PLAYERS);
+
+    return EXIT_SUCCESS;
+}
---
+++ questions.c
@@ -0,0 +1,86 @@
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "questions.h"
+
+question questions[NUM_QUESTIONS][NUM_VALUES];
+
+// Initializes the array of questions for the game
+void initialize_game(void)
+{
+    // initialize each question struct and assign it to the questions array
+    for (int i = 0; i < NUM_CATEGORIES; i++) {
+        for (int j = 0; j < NUM_VALUES; j++) {
+            question q;
+            sprintf(q.category, "Category %d", i + 1);
+            sprintf(q.question, "Question %d worth $%d", i + 1, (j + 1) * 100);
+            sprintf(q.answer, "Answer %d", i + 1);
+            q.value = (j + 1) * 100;
+            q.answered = false;
+            questions[i][j] = q;
+        }
+    }
+}
+
+// Displays each of the remaining categories and question dollar values that have not been answered
+void display_categories(void)
+{
+    // print categories and dollar values for each unanswered question in questions array
+    printf("Remaining Categories and Question Dollar Values:\n");
+    for (int i = 0; i < NUM_CATEGORIES; i++) {
+        for (int j = 0; j < NUM_VALUES; j++) {
+            if (!questions[i][j].answered) {
+                printf("%s: $%d\t", questions[i][j].category, questions[i][j].value);
+            }
+        }
+        printf("\n");
+    }
+}
+
+// Displays the question for the category and dollar value
+void display_question(char *category, int value)
+{
+    for (int i = 0; i < NUM_CATEGORIES; i++) {
+        for (int j = 0; j < NUM_VALUES; j++) {
+            if (strcmp(questions[i][j].category, category) == 0 && questions[i][j].value == value) {
+                printf("Question: %s\n", questions[i][j].question);
+                return;
+            }
+        }
+    }
+}
+
+// Returns true if the answer is correct for the question for that category and dollar value
+bool valid_answer(char *category, int value, char *answer)
+{
+    // Look into string comparison functions
+    for (int i = 0; i < NUM_CATEGORIES; i++) {
+        for (int j = 0; j < NUM_VALUES; j++) {
+            if (strcmp(questions[i][j].category, category) == 0 && questions[i][j].value == value) {
+                return strcmp(questions[i][j].answer, answer) == 0;
+            }
+        }
+    }
+    return false;
+}
+
+// Returns true if the question has already been answered
+bool already_answered(char *category, int value)
+{
+    // lookup the question and see if it's already been marked as answered
+    for (int i = 0; i < NUM_CATEGORIES; i++) {
+        for (int j = 0; j < NUM_VALUES; j++) {
+            if (strcmp(questions[i][j].category, category) == 0 && questions[i][j].value == value) {
+                return questions[i][j].answered;
+            }
+        }
+    }
+    return false;
+}
